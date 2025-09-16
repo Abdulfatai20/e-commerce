873d6c76b5947d4311644caa3ea305ae67b0f4a4
unknown <abdulfataimahmood35@gmail.com>
2025-09-16 20:47:59 +0100

    Initial commit

---
+++ src/components/Layout.jsx
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import {ToastContainer, toast} from 'react-toastify';
+
+const Layout = () => {
+  return (
+    <>
+    <ToastContainer/>
+      <Navbar />
+      <SearchBar/>
+      <Outlet />
+      <Footer/>
+    </>
+  );
+};
+
+export default Layout;
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ShopContextProvider from "./context/ShopContext.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <ShopContextProvider>
+      <Routes>
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </ShopContextProvider>
+  </BrowserRouter>
+);
